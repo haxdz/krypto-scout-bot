ede0ad5d49618a45fbbc57d22426936e29b502d7
Данил Петраков <danilpetrakov@MacBookPro.cablenet.com.cy>
2025-05-31 00:27:01 +0300

    add new features

--- scheduler.py
+++ scheduler.py
@@ -1,22 +1,17 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from signals import check_market_and_notify
+import asyncio
 
 scheduler = AsyncIOScheduler()
-scheduler.add_job(check_market_and_notify, 'interval', seconds=900)  # каждый 15 минут
-
-def start():
-    scheduler.start()
 
 def my_task(app):
-    print("🔄 Выполнена тестовая задача!")
+    print("⚙️ Выполнена тестовая задача!")
 
 def setup_jobs(app):
     scheduler.add_job(check_market_and_notify, 'interval', seconds=900, args=[app])
 
-import asyncio
-
 def start_scheduler(app):
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(my_task, 'interval', seconds=10, args=[app])
+    scheduler.add_job(check_market_and_notify, 'interval', seconds=900, args=[app])
     loop = asyncio.get_event_loop()
     loop.create_task(scheduler.start())
